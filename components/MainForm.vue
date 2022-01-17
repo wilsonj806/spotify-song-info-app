630d04bfe5bb5253759a15a49c0913ea1e13ba56
Wilson <wilsonj806@gmail.com>
2022-01-16 23:49:49 -0500

    add data fetch, add somewhat dynamic form

---
+++ components/MainForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1></h1>
+    <form>
+      <label>Spotify song URL</label>
+      <input type='text' @change='emitInputChange'>
+      <button>Submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    emitInputChange(e) {
+      this.$emit('callChangeHandler',e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <Tutorial/>
+<main>
+  <MainForm @callChangeHandler='onEmitInputChange'/>
+  <p>{{songUrl}}</p>
+</main>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+    data() {
+      return {
+          songUrl: '',
+          artistData: null,
+          errors: []
+      }
+    },
+    methods: {
+      async submitForm() {
+          await axios.post('http://localhost:3000/songinfo',{songUrl: this.songUrl})
+            .then(res => {
+              this.artistData = res.json()
+              })
+            .catch(e => {
+              console.dir(e)
+              this.errors.push(e)
+            })
+      },
+      onEmitInputChange(value) {
+        this.songUrl = value
+      }
+    }
+}
 </script>
